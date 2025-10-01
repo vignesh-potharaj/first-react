4ac1e3f1128ac408f6bce542d59dc60074d8bf35
vignes-potharaj <freelancer.nenu@gmail.com>
2025-10-01 12:40:28 +0530

    feat: Used React classes to create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-const continents = ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Australia', 'South America'];
-const helloContinents = Array.from(continents, c => `Hello! ${c}!`);
-const message = helloContinents.join('');
+class HelloWorld extends React.Component {
+    render() {
+        const continents = ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Australia', 'South America'];
+        const helloContinents = Array.from(continents, c => `Hello, ${c}!`);
+        const message = helloContinents.join(' ');
 
-const element = (
-    <div title = 'Outer div'>
-        <h1>{message}</h1>
-    </div>
-);
+        return (
+            <div title = 'Outer div'>
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
+
+const element = <HelloWorld />;
+
 ReactDOM.render(element, document.getElementById('content'));
